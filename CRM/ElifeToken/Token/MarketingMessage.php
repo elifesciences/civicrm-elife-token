49b558b51e92711f66f9c428614b30a437c9db95
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-09-21 19:25:02 +0100

    adding missing param

--- CRM/ElifeToken/Token/MarketingMessage.php
+++ CRM/ElifeToken/Token/MarketingMessage.php
@@ -21,9 +21,9 @@ class CRM_ElifeToken_Token_MarketingMessage{
       $civinky = new CRM_ElifeToken_Civinky;
 
       $css = file_get_contents(CIVICRM_UF_BASEURL."sites/all/libraries/elife-newsletter-assets/newsletter.css");
-      $html = 'columns: p.promo '.json_decode(file_get_contents(CIVICRM_UF_BASEURL."newsletters/marketing-message/$type"), true)['html'];
+      $html = "columns: p.promo ".json_decode(file_get_contents(CIVICRM_UF_BASEURL."newsletters/marketing-message/$type"), true)['html'];
 
-      $this->token[$type] = $civinky->query($html, $css, true);
+      $this->token[$type] = $civinky->query($html, [], $css, true);
     }
     return $this->token[$type];
   }
