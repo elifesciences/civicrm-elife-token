c71d9f65f4754daee69054dafd36957c012427ca
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-21 11:27:16 +0100

    Added basic smoke test

---
+++ test/CRM/ElifeToken/Token/ArticlesLast7DaysTest.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once __DIR__.'/../../../../CRM/ElifeToken/Token/ArticlesLast7Days.php';
+
+class CRM_ElifeToken_Token_ArticlesLast7DaysTest extends PHPUnit_Framework_TestCase {
+  public static function possibleTypes() {
+    return [
+        ['magazine'],
+    ];
+  }
+  /**
+   * @dataProvider possibleTypes
+   * @test
+   */
+  public function smokeTestTheContentFilteredFor($type) {
+    $token = new CRM_ElifeToken_Token_ArticlesLast7Days();
+    $content = $token->getArticles('magazine');
+    $this->assertInternalType('array', $content);
+    $this->assertArrayHasKey('total', $content);
+    $this->assertInternalType('integer', $content['total']);
+    $this->assertGreaterThan(0, $content['total']);
+    $this->assertArrayHasKey('items', $content);
+    $items = $content['items'];
+    $this->assertInternalType('array', $items);
+    foreach ($items as $item) {
+        $this->assertInternalType('array', $item);
+    }
+  } 
+}
