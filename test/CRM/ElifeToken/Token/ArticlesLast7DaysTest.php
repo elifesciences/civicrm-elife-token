867fee2df40291338f61fc07e2403dd988502d47
Giorgio Sironi <g.sironi@elifesciences.org>
2017-09-21 14:09:00 +0100

    Updated test to cover 3 tokens: poa, vor, magazine

--- test/CRM/ElifeToken/Token/ArticlesLast7DaysTest.php
+++ test/CRM/ElifeToken/Token/ArticlesLast7DaysTest.php
@@ -3,21 +3,49 @@
 require_once __DIR__.'/../../../../CRM/ElifeToken/Token/ArticlesLast7Days.php';
 
 class CRM_ElifeToken_Token_ArticlesLast7DaysTest extends PHPUnit_Framework_TestCase {
-  public static function possibleTypes() {
-    return [
-        // not sure these are the right inputs, but they would 
-        // be a good naming convention
-        ['poa'],
-        ['vor'],
-    ];
+  public function setUp() {
+    $this->token = new CRM_ElifeToken_Token_ArticlesLast7Days();
+    $this->magazineTypes = ['editorial', 'feature', 'insight'];
   }
+
   /**
-   * @dataProvider possibleTypes
    * @test
    */
-  public function smokeTestTheContentFilteredFor($type) {
-    $token = new CRM_ElifeToken_Token_ArticlesLast7Days();
-    $content = $token->getArticles($type);
+  public function smokeTestThePoaContent() {
+    $content = $this->token->getArticles('poa');
+    $this->checkDataStructure($content);
+    foreach ($items as $item) {
+      $this->assertArrayHasKey('status', $item);
+      $this->assertSame($type, $item['status']);
+    }
+  }
+
+  /**
+   * @test
+   */
+  public function smokeTestTheVorContent() {
+    $content = $this->token->getArticles('vor');
+    $this->checkDataStructure($content);
+    foreach ($items as $item) {
+      $this->assertSame('vor', $item['status']);
+      $this->assertNotContains($item['type'], $this->magazineTypes);
+    }
+  }
+
+  /**
+   * @test
+   */
+  public function smokeTestTheMagazineContent() {
+    $content = $this->token->getArticles('vor');
+    $this->checkDataStructure($content);
+    foreach ($items as $item) {
+      $this->assertArrayHasKey('status', $item);
+      $this->assertSame('vor', $item['status']);
+      $this->assertContains($item['type'], $this->magazineTypes);
+    }
+  }
+
+  private function checkDataStructure($content) {
     $this->assertInternalType('array', $content);
     $this->assertArrayHasKey('total', $content);
     $this->assertInternalType('integer', $content['total']);
@@ -26,9 +54,8 @@ class CRM_ElifeToken_Token_ArticlesLast7DaysTest extends PHPUnit_Framework_TestC
     $items = $content['items'];
     $this->assertInternalType('array', $items);
     foreach ($items as $item) {
-        $this->assertInternalType('array', $item);
-        $this->assertArrayHasKey('status', $item);
-        $this->assertSame($type, $item['status']);
+      $this->assertInternalType('array', $item);
+      $this->assertArrayHasKey('status', $item);
     }
   } 
 }
