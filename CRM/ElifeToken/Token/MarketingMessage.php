5be4408aebe87f956b6f5116b78d199ce117abbe
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-09-22 10:08:50 +0100

    add tracking token

--- CRM/ElifeToken/Token/MarketingMessage.php
+++ CRM/ElifeToken/Token/MarketingMessage.php
@@ -23,6 +23,9 @@ class CRM_ElifeToken_Token_MarketingMessage{
       $css = file_get_contents(CIVICRM_UF_BASEURL."sites/all/libraries/elife-newsletter-assets/newsletter.css");
       $html = "columns: p.promo ".json_decode(file_get_contents(CIVICRM_UF_BASEURL."newsletters/marketing-message/$type"), true)['html'];
 
+      $trackingToken = CRM_ElifeToken_Token_GATracking::Instance();
+      $html = str_replace('{track}', $trackingToken->get(), $html);
+
       $this->token[$type] = $civinky->query($html, [], $css, true);
     }
     return $this->token[$type];
